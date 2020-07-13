32bec639afb660186938199c6281db549874a034
Nicolas <expectopatrom@gmail.com>
2020-07-12 19:58:21 -0500

    First commit

---
+++ .config/scripts/startup.sh
@@ -0,0 +1,11 @@
+#Turn on shortcuts xbindkeys
+xbindkeys  
+#Changes layout
+setxkbmap -layout latam
+#Screen Configuration
+redshift -x
+redshift -b :0.5
+redshift -O :3800
+#Wallpaper 
+feh --bg-scale "/home/nico/Images/Wallpapers/city.png"
+
